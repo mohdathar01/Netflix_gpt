d3b3cd82ec37fe5fcda0a1e08947c8590470bee0
unknown <atharchaudhary2000@gmail.com>
2024-10-03 23:06:23 +0530

    component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import "./Header";
 
 function App() {
   return (
     <div className="text-3xl font-bold text-green-800">
-      Namaste chatgpt netflix
+       {<Header/>}
     </div>
   );
 }
